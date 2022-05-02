86ff973ea040116f6dce2c73faa7c0e9ca7cb450
MohammadGhandour <mohammadghandour02@outlook.com>
2022-05-02 15:33:01 +0300

    Ajout du port BDD par défaut

--- back/config/dbConfig.js
+++ back/config/dbConfig.js
@@ -5,7 +5,8 @@ const db = mysql.createConnection({
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
-    database: process.env.DB
+    database: process.env.DB,
+    port: process.env.PORT
 });
 
 module.exports = db
